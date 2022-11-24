6ff2f269a9cdb8600fb32447de3a6309a12ff5c6
Pascual Box <pascualbox1@gmail.com>
2022-11-24 12:42:17 +0100

    SignUp screen created

--- Front-end/src/index.js
+++ Front-end/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Form from './components/Form/Form';
+import Signup from './pages/Signup/Signup';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Form />
+    <Signup />
   </React.StrictMode>
 );
 
