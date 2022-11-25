7891c5e61f6800371443455396fc9cd5d3a3eeaf
Rubén Palomo Fontán <ruben.palomof@gmail.com>
2022-11-25 10:40:24 +0100

    Corrections and fixes

--- Front-end/src/index.js
+++ Front-end/src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import Signup from './pages/Signup/Signup';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Signup from "./pages/Signup/Signup";
+import reportWebVitals from "./reportWebVitals";
+import Header from "./Components/Header/Header";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Header />
     <Signup />
   </React.StrictMode>
 );
