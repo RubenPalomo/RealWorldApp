f05be5dba0cb8de9dbb1eafb11380086488246aa
JoseMartinCalvo <117278127+JoseMartinCalvo@users.noreply.github.com>
2022-11-28 15:27:19 +0100

    Read/Unread icon

--- Front-end/src/Components/Notification/Notification.jsx
+++ Front-end/src/Components/Notification/Notification.jsx
@@ -1,24 +1,33 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './Notification.scss'
-import unreadIcon from '../../assets/unread-icon.png'
-import readIcon from '../../assets/read-icon.png'
-import { useState } from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Notification.scss";
+import unreadIcon from "../../assets/unread-icon.png";
+import readIcon from "../../assets/read-icon.png";
+import { useState } from "react";
 
 function Notification(props) {
-    const [isRead, setisRead] = useState(true)
-    function setRead(){
-        setisRead(!isRead);
-    }    
+  const [isRead, setisRead] = useState(true);
+  function setRead() {
+    setisRead(!isRead);
+  }
   return (
-
-    <div className={isRead ? "notificationComponentMainDiv" : "notificationComponentMainDiv notRead"}>
-        <div className='notificationComponentTextDiv'>Hi{props.text}</div>
-        <div className='notificationComponentImageDiv'><img src={isRead ? unreadIcon : readIcon} onClick={setRead} width="48px"></img></div>
+    <div
+      className={
+        isRead
+          ? "notificationComponentMainDiv"
+          : "notificationComponentMainDiv notRead"
+      }
+    >
+      <div className="notificationComponentTextDiv">HI{props.text}</div>
+      <div className="notificationComponentImageDiv">
+        <img
+          src={isRead ? unreadIcon : readIcon}
+          onClick={setRead}
+          width={isRead ? "48px" : "45px"}
+        ></img>
+      </div>
     </div>
-    
-    
-  )
+  );
 }
 
-export default Notification
+export default Notification;
